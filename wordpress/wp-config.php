50a2a6e5a2e1626159aaa6a1aef3a199d599c29b
zoeyduku <zoeyducoeur@gmail.com>
2016-02-26 00:05:17 -0500

    a

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'zoeyduku');
 define('DB_PASSWORD', 'P1ckl35');
 
 /** MySQL hostname */
-define('DB_HOST', 'https://github/zoeyduku/BoilerKings.git');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
